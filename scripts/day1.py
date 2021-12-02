a3241ec6f2a281b98c5b458fe8f8dee695ce5eba
Brian Schaefer <physinet@gmail.com>
2021-12-02 00:01:32 -0500

    day 1 part 2

--- scripts/day1.py
+++ scripts/day1.py
@@ -1,14 +1,17 @@
 import pathlib
 
-def part1() -> int:
+def number_of_measurement_increases(window=1) -> int:
     input_file = pathlib.Path(__file__).resolve().parent.parent / "input/day1"
     with open(input_file, "r") as f:
         depths = [int(row) for row in f.readlines()]
+
     num_increases = sum(
-        depth2 > depth1 for depth1, depth2 in zip(depths, depths[1:])
+        sum(depths[i+1:i+window+1]) > sum(depths[i:i+window]) 
+        for i in range(len(depths)-window)
     )
     return num_increases
 
 if __name__ == "__main__":
-    print(part1())
+    print(number_of_measurement_increases(1))
+    print(number_of_measurement_increases(3))
 
