303b5e0b03658925ca70748c5ea1983dabe8ce5f
Brian Schaefer <physinet@gmail.com>
2021-12-05 10:20:27 -0500

    day 5 part 2

--- scripts/day5.py
+++ scripts/day5.py
@@ -1,7 +1,12 @@
+from itertools import zip_longest
+from math import copysign
 from typing import Iterable
+
 from helpers import get_input
 
 Coordinate = tuple[int, int]
+
+sign = lambda x: int(copysign(1, x))
 
 
 def get_coords(rows: Iterable[str]) -> list[tuple[Coordinate, Coordinate]]:
@@ -11,24 +16,24 @@ def get_coords(rows: Iterable[str]) -> list[tuple[Coordinate, Coordinate]]:
     ]
 
 
-def sign(x: bool) -> int:
-    """Returns -1 if x is False, 1 if x is True"""
-    return 2 * x - 1
-
-
-def main() -> int:
+def main(diagonal: bool = False) -> int:
     coords = get_coords(get_input(5))
     N = 1000
     board = [[0 for _ in range(N)] for _ in range(N)]
+
     for start, end in coords:
-        if (x := start[0]) == end[0]:
-            direction =  sign(end[1] > start[1])
-            for j in range(start[1], end[1] + direction, direction):
-                board[x][j] += 1
-        elif (y := start[1]) == end[1]:
-            direction =  sign(end[0] > start[0])
-            for i in range(start[0], end[0] + direction, direction):
-                board[i][y] += 1
+        # amount to iterate in each direction
+        x_iter, y_iter = (end[i] - start[i] for i in range(2))
+        if diagonal or any(i == 0 for i in (x_iter, y_iter)):
+            x_dir, y_dir = sign(x_iter), sign(y_iter)
+            for i, j in zip_longest(
+                range(start[0], end[0] + x_dir, x_dir),
+                range(start[1], end[1] + y_dir, y_dir)
+            ):
+                i = start[0] if i is None else i
+                j = start[1] if j is None else j
+
+                board[i][j] += 1
 
     count_overlap = 0
     for row in board:
@@ -40,3 +45,4 @@ def main() -> int:
 
 if __name__ == "__main__":
     print(main())
+    print(main(diagonal=True))
