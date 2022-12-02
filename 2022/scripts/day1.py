941eedb5e9a0369305ebfda125ebbd08ee20f5e3
Brian Schaefer <physinet@gmail.com>
2022-12-01 21:38:36 -0500

    day1 part 1

--- 2022/scripts/day1.py
+++ 2022/scripts/day1.py
@@ -1,15 +1,18 @@
 from helpers import get_input
 
-def number_of_measurement_increases(window=1) -> int:
-    depths = [int(row) for row in get_input(1)]
 
-    num_increases = sum(
-        sum(depths[i+1:i+window+1]) > sum(depths[i:i+window]) 
-        for i in range(len(depths)-window)
-    )
-    return num_increases
+def calories() -> int:
+    max_calories = -1
+    running_total = 0
+    for row in get_input(1):
+        if cals := row.strip():
+            running_total += int(cals)
+        else:
+            max_calories = max(max_calories, running_total)
+            running_total = 0
+
+    return max_calories
+
 
 if __name__ == "__main__":
-    print(number_of_measurement_increases(1))
-    print(number_of_measurement_increases(3))
-
+    print(calories())
