fc08364b49d36915c0d990fd12eb4b48554e3425
Brian Schaefer <physinet@gmail.com>
2022-12-02 21:04:11 -0500

    day2 part2

--- 2022/scripts/day2.py
+++ 2022/scripts/day2.py
@@ -13,13 +13,28 @@ SCORES = {
 }
 
 
-def rps() -> int:
+SCORES2 = {
+    ("A", "X"): 0 + 3,
+    ("A", "Y"): 3 + 1,
+    ("A", "Z"): 6 + 2,
+    ("B", "X"): 0 + 1,
+    ("B", "Y"): 3 + 2,
+    ("B", "Z"): 6 + 3,
+    ("C", "X"): 0 + 2,
+    ("C", "Y"): 3 + 3,
+    ("C", "Z"): 6 + 1,
+}
+
+
+def rps(rows, scores) -> int:
     score = 0
-    for row in get_input(2):
-        opponent, you = row.split()
-        score += SCORES[opponent, you]
+    for row in rows:
+        score += scores[tuple(row.split())]
     return score
 
 
 if __name__ == "__main__":
-    print(rps())
+    print(rps(["A Y", "B X", "C Z"], SCORES))
+    print(rps(get_input(2), SCORES))
+    print(rps(["A Y", "B X", "C Z"], SCORES2))
+    print(rps(get_input(2), SCORES2))
