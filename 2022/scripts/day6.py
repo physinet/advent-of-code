91a9d59d71ee909ea5eb7aad8983371456648989
Brian Schaefer <physinet@gmail.com>
2022-12-06 18:37:54 -0500

    day6 part2

--- 2022/scripts/day6.py
+++ 2022/scripts/day6.py
@@ -2,26 +2,28 @@ from helpers import get_input
 from collections import Counter
 
 TEST_CASES = (
-    ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7),
-    ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
-    ("nppdvjthqldpwncqszvftbrmjlhg", 6),
-    ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
-    ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
+    ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7, 19),
+    ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5, 23),
+    ("nppdvjthqldpwncqszvftbrmjlhg", 6, 23),
+    ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10, 29),
+    ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11, 26),
 )
 
 
-def main(text):
-    counter = Counter(text[:4])
-    i = 4
-    while sum(x != 0 for x in counter.values()) != 4:
+def main(text, N):
+    counter = Counter(text[:N])
+    i = N
+    while sum(x != 0 for x in counter.values()) != N:
         counter[text[i]] += 1
-        counter[text[i - 4]] -= 1
+        counter[text[i - N]] -= 1
         i += 1
     return i
 
 
 if __name__ == "__main__":
-    for case, result in TEST_CASES:
-        assert main(case) == result
+    for case, result4, result14 in TEST_CASES:
+        assert main(case, 4) == result4
+        assert main(case, 14) == result14
     (text,) = get_input(6)
-    print(main(text))
+    print(main(text, 4))
+    print(main(text, 14))
