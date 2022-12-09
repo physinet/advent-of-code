1d8e5ecdc6b45a3e5ecd1ed3555f1e467b4fcaa6
Brian Schaefer <physinet@gmail.com>
2022-12-09 18:45:58 -0500

    day 8 part 2

--- 2022/scripts/day8.py
+++ 2022/scripts/day8.py
@@ -1,4 +1,5 @@
 from helpers import get_input
+from functools import reduce
 
 TEST = """30373
 25512
@@ -45,13 +46,56 @@ def num_trees_visible(vis_map: list[list[bool]]) -> int:
     return sum(is_visible for row in vis_map for is_visible in row)
 
 
+def max_scenic_score(tree_map: list[list[int]]) -> int:
+    n_row = len(tree_map)
+    n_col = len(tree_map[0])
+    scores = [[[] for _ in range(n_col)] for _ in range(n_row)]
+    for i, row in enumerate(tree_map):
+        for j, col in enumerate(tree_map):
+            h_tree = tree_map[i][j]
+            four_score = []
+            tot = -1
+            for ii in range(i, -1, -1):
+                tot += 1
+                if tree_map[ii][j] >= h_tree and i != ii:
+                    break
+            four_score.append(tot)
+            tot = -1
+            for ii in range(i, n_row):
+                tot += 1
+                if tree_map[ii][j] >= h_tree and i != ii:
+                    break
+            four_score.append(tot)
+            tot = -1
+            for jj in range(j, -1, -1):
+                tot += 1
+                if tree_map[i][jj] >= h_tree and j != jj:
+                    break
+            four_score.append(tot)
+            tot = -1
+            for jj in range(j, n_col):
+                tot += 1
+                if tree_map[i][jj] >= h_tree and j != jj:
+                    break
+            four_score.append(tot)
+            scores[i][j] = four_score
+    total_scores = [
+        reduce(lambda x, y: x * y, four_score, 1)
+        for row in scores
+        for four_score in row
+    ]
+    return max(total_scores)
+
+
 if __name__ == "__main__":
     tree_map = process_input(TEST.split("\n"))
     # print(tree_map)
     vis_map = get_visible_map(tree_map)
     # print(vis_map)
     assert num_trees_visible(vis_map) == 21
+    assert max_scenic_score(tree_map) == 8
 
     tree_map = process_input(get_input(8))
     vis_map = get_visible_map(tree_map)
     print(num_trees_visible(vis_map))
+    print(max_scenic_score(tree_map))
