c65cbab3a03e28a0441d9494f4a3bfd401a6b7f6
Brian Schaefer <physinet@gmail.com>
2022-12-10 16:16:36 -0500

    refactor for part 2

--- 2022/scripts/day9.py
+++ 2022/scripts/day9.py
@@ -12,44 +12,45 @@ R 2"""
 MOVES = {"L": (-1, 0), "R": (1, 0), "U": (0, 1), "D": (0, -1)}
 
 
-def _tail_too_far(head_position, tail_position):
-    return any(abs(head_position[i] - tail_position[i]) > 1 for i in range(2))
+def _follower_too_far(leader, follower):
+    return any(abs(leader[i] - follower[i]) > 1 for i in range(2))
 
 
-def _draw(head_position, tail_position, N=5):
+def _draw(positions, N=5):
     s = ""
     for i in range(N - 1, -1, -1):
         for j in range(N):
-            if head_position == (j, i):
-                s += "H"
-            elif tail_position == (j, i):
-                s += "T"
+            for k, position in enumerate(positions):
+                if position == (j, i):
+                    s += "H" if k == 0 else str(k)
+                    break
             else:
                 s += "."
         s += "\n"
     print(s)
 
 
-def _move(head_position, tail_position, move):
-    head_position = head_position[0] + move[0], head_position[1] + move[1]
-    if _tail_too_far(head_position, tail_position):
-        # tail position moves to follow the head in the opposite direction the head moved
-        tail_position = head_position[0] - move[0], head_position[1] - move[1]
-    return head_position, tail_position
+def _move(positions, move):
+    # Head
+    positions[0] = positions[0][0] + move[0], positions[0][1] + move[1]
+    for i, (leader, follower) in enumerate(zip(positions[:-1], positions[1:])):
+        if _follower_too_far(leader, follower):
+            # follower position moves to follow the leader in the opposite direction the leader moved
+            positions[i + 1] = leader[0] - move[0], leader[1] - move[1]
+    return positions
 
 
-def main(rows, draw=False):
-    head_position = (0, 0)
-    tail_position = (0, 0)
+def main(rows, N=2, draw=False):
+    positions = [(0, 0) for _ in range(N)]
     tail_visited = set()
     for row in rows:
         direction, distance = row.strip().split()
         move = MOVES[direction]
         for _ in range(int(distance)):
             if draw:
-                _draw(head_position, tail_position)
-            head_position, tail_position = _move(head_position, tail_position, move)
-            tail_visited.add(tuple(tail_position))
+                _draw(positions)
+            positions = _move(positions, move)
+            tail_visited.add(positions[-1])
     return len(tail_visited)
 
 
