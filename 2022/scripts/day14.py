223f79ff18af9b3288cda6a2bc78b964c0770475
Brian Schaefer <physinet@gmail.com>
2022-12-19 17:30:03 -0500

    day14 parsing

---
+++ 2022/scripts/day14.py
@@ -0,0 +1,59 @@
+from helpers import get_input
+
+TEST = """498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9"""
+TEST_COORDS = {
+    (498, 4), 
+    (498, 5), 
+    (498, 6), 
+    (497, 6), 
+    (496, 6), 
+    (503, 4), 
+    (502, 4), 
+    (502, 5),
+    (502, 6),
+    (502, 7),
+    (502, 8),
+    (502, 9),
+    (501, 9),
+    (500, 9),
+    (499, 9),
+    (498, 9),
+    (497, 9),
+    (496, 9),
+    (495, 9),
+    (494, 9),
+}
+
+
+Coordinate = tuple[int, int]
+
+def parse(rows: list[str]) -> list[list[Coordinate]]:
+    return [
+        [
+            (int(x), int(y))
+            for x, y in coord.split(",")
+            for coord in row.split(" -> ") 
+        ]
+        for row in rows
+    ]
+
+def sign(a):
+    return a // abs(a)
+
+def build_walls(walls: list[list[Coordinate]]) -> set[Coordinate]:
+    return {
+        (i, j)
+        for wall in walls
+        for (x1, y1), (x2, y2) in zip(wall[:-1], wall[1:])
+        for i in range(x1, x2, sign(x2 - x1))
+        for j in range(y1, y2, sign(y2 - y1))
+    }
+
+def main(coords: set[Coordinate], source: Coordinate) -> None:
+    ...
+
+if __name__ == "__main__":
+    walls = parse(TEST.split("\n"))
+    coords = build_walls(walls)
+    assert coords == TEST_COORDS
