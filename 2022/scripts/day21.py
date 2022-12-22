fb8afbcf47adaca87068b4414a5affec8a1f3e31
Brian Schaefer <physinet@gmail.com>
2022-12-22 04:07:43 +0000

    day21 part2

--- 2022/scripts/day21.py
+++ 2022/scripts/day21.py
@@ -27,6 +27,13 @@ OPERATORS = {
     "/": operator.floordiv,
 }
 
+INV_OPERATORS = {
+    "-": operator.add,
+    "+": operator.sub,
+    "/": operator.mul,
+    "*": operator.floordiv,
+}
+
 
 @dataclass
 class Operation:
@@ -52,16 +59,57 @@ def parse(rows: list[str]) -> dict:
 def get(jobs: dict, name: str):
     if isinstance(jobs[name], int):
         return jobs[name]
-    else:
+    elif isinstance(jobs[name], Operation):
         operation = jobs[name]
         return OPERATORS[operation.op](
             get(jobs, operation.name1), get(jobs, operation.name2)
         )
 
 
+def inverse(jobs, val, name):
+    operation = jobs[name]
+    unsafe_branch = None
+    for name in (operation.name1, operation.name2):
+        try:
+            if get(jobs, name) is not None:
+                safe_branch = name
+        except TypeError:
+            unsafe_branch = name
+
+    if (operation.name1 == safe_branch) and (operation.op in "-/"):
+        val = OPERATORS[operation.op](get(jobs, safe_branch), val)
+    else:
+        val = INV_OPERATORS[operation.op](val, get(jobs, safe_branch))
+
+    if unsafe_branch is None:
+        return val
+
+    return inverse(jobs, val, unsafe_branch)
+
+
+def main2(jobs: dict) -> int:
+    # Replace the human with None
+    jobs["humn"] = None
+    root_op = jobs["root"]
+
+    # Find the branch that contains the human
+    for name in (root_op.name1, root_op.name2):
+        try:
+            get(jobs, name)
+            safe_branch = name
+        except TypeError:
+            unsafe_branch = name
+
+    val = get(jobs, safe_branch)
+    return inverse(jobs, val, unsafe_branch)
+
+
 if __name__ == "__main__":
     jobs = parse(TEST.split("\n"))
     assert get(jobs, "root") == 152
+    assert main2(jobs) == 301
 
     jobs = parse(get_input(21))
     print(get(jobs, "root"))
+    print(main2(jobs))
+    breakpoint()
